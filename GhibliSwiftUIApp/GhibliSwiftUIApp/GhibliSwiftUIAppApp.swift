a82f8ba03a57b1cff4054a05ec29fe40b3d70da5
jancy123 <jancelots@gmail.com>
2026-02-09 11:40:45 -0800

    Created Ghible project swift concurrency set to target

---
+++ GhibliSwiftUIApp/GhibliSwiftUIApp/GhibliSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIAppApp.swift
+//  GhibliSwiftUIApp
+//
+//  Created by Jancy on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
