a78821540e2fb7a91be4fd878d056709d95f6e40
jancy123 <jancelots@gmail.com>
2026-02-24 20:42:48 -0800

    Added code to set state and to set error in a failure

--- GhibliSwiftUIApp/GhibliSwiftUIApp/Networking/FilmsViewModel.swift
+++ GhibliSwiftUIApp/GhibliSwiftUIApp/Networking/FilmsViewModel.swift
@@ -9,29 +9,49 @@ import Observation
 
 enum APIError: LocalizedError {
     case invalidURL
-    case decodingFailed
+    case invalidResponse
+    case decoding(Error)
     case networkError(Error)
 }
 
 @Observable
 class FilmsViewModel {
     
-    enum State {
+    enum State: Equatable {
         case idle
         case loading
         case loaded([Film])
         case error(String)
     }
     var films: [Film] = []
-    
-    func fetchFilms() async throws {
-        guard let url = URL(string: "https://ghibliapi.vercel.app/films") else { throw APIError.invalidURL }
+    var state: State = .idle
+    func fetch() async {
+        guard state == .idle else { return }
+        state = .idle
+        do {
+            let films = try await fetchFilms()
+            self.state = .loaded(films)
+            self.state = .loading
+        } catch {
+            
+        }
+    }
+    private func fetchFilms() async throws -> [Film] {
+        guard let url = URL(string: "https://ghibliapi.vercel.app/films") else {
+            throw APIError.invalidURL }
         do {
             
                 let (data, response) = try await URLSession.shared.data(from: url)
-                films = try JSONDecoder().decode([Film].self, from: data)
-        } catch {
-         print(error)
-     }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                throw APIError.invalidResponse
+            }
+            
+                return try JSONDecoder().decode([Film].self, from: data)
+        } catch let error as DecodingError {
+            throw APIError.decoding(error)
+        } catch let error as URLError {
+            throw APIError.networkError(error)
+        }
     }
 }
